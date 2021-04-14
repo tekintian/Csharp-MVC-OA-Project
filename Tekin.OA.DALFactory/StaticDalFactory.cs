cb3f089e70c23d3efe4ce357dad4de035ed8dc4f
tekintian <tekintian@gmail.com>
2021-04-14 20:39:08 +0800

    抽象工厂模式实现
    Signed-off-by: tekintian <tekintian@gmail.com>

--- Tekin.OA.DALFactory/StaticDalFactory.cs
+++ Tekin.OA.DALFactory/StaticDalFactory.cs
@@ -1,19 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Configuration;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using Tekin.OA.EFDAL;
 using Tekin.OA.IDAL;
 
 namespace Tekin.OA.DALFactory
 {
     /// <summary>
-    /// 简单工厂设计模式
+    /// 设计模式
+    /// 简单工厂  需要修改new实现代码
+    /// 抽象工厂  使用反射将new放入配置,不需要修改实现代码
     /// </summary>
     public class StaticDalFactory
     {
+        private static string assemblyName = ConfigurationManager.AppSettings["DalAssemblyName"];
         //返回接口类
         public static IUserInfoDal GetUserInfoDal()
         {
@@ -21,13 +20,14 @@ namespace Tekin.OA.DALFactory
             //return new UserInfoDal();
 
             //使用反射的方式获取对象 
-            // 在UI 修改配置文件的实例程序集和接口名称
-            return  Assembly.Load("Tekin.OA.EFDAL").CreateInstance("UserInfoDal") as IUserInfoDal;
-        }
+            // 在UI 修改配置文件的实例程序集名称
 
+            return Assembly.Load(assemblyName).CreateInstance("UserInfoDal") as IUserInfoDal;
+        }
+        //抽象工厂模式实现
         public static IOrderInfoDal GetOrderInfoDal()
         {
-            return new OrderInfoDal();
+            return Assembly.Load(assemblyName).CreateInstance("IOrderInfoDal") as IOrderInfoDal;
         }
     }
 }
