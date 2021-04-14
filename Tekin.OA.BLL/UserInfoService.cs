9cb01fab9eae32e204615cdf2cff6509d11239fd
tekintian <tekintian@gmail.com>
2021-04-14 19:09:25 +0800

    增删改业务逻辑层
    Signed-off-by: tekintian <tekintian@gmail.com>

---
+++ Tekin.OA.BLL/UserInfoService.cs
@@ -0,0 +1,26 @@
+﻿using Tekin.OA.EFDAL;
+using Tekin.OA.Model;
+
+namespace Tekin.OA.BLL
+{
+    
+    public class UserInfoService
+    {
+        UserInfoDal userDal = new UserInfoDal();
+        //新增
+        public UserInfo Add(UserInfo user)
+        {
+            return userDal.Add(user);
+        }
+        //修改
+        public bool Update(UserInfo user)
+        {
+            return userDal.Update(user);
+        }
+        //删除
+        public bool Delete(UserInfo user)
+        {
+            return userDal.Delete(user);
+        }
+    }
+}
