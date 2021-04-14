43205dc4da7b8205d87927919bfdaa2dc4bab344
tekintian <tekintian@gmail.com>
2021-04-14 19:08:44 +0800

    增删改查子类
    Signed-off-by: tekintian <tekintian@gmail.com>

---
+++ Tekin.OA.EFDAL/OrderInfoDal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tekin.OA.Model;
+
+namespace Tekin.OA.EFDAL
+{
+    class OrderInfoDal:BaseDal<OrderInfo>
+    {
+    }
+}
---
+++ Tekin.OA.EFDAL/UserInfoDal.cs
@@ -0,0 +1,8 @@
+﻿using Tekin.OA.Model;
+
+namespace Tekin.OA.EFDAL
+{
+    public class UserInfoDal:BaseDal<UserInfo>
+    {
+    }
+}
